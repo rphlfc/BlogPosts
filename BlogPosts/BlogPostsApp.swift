860647e16f1ec1c1089ddf4ff461e720c9886279
Raphael Cerqueira <rcerqueira@vr.com.br>
2020-11-24 17:37:22 -0300

    Initial Commit

---
+++ BlogPosts/BlogPostsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlogPostsApp.swift
+//  BlogPosts
+//
+//  Created by Raphael Cerqueira on 24/11/20.
+//
+
+import SwiftUI
+
+@main
+struct BlogPostsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
